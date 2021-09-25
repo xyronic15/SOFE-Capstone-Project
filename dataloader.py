64010f0b57886da0a90a993829430f9dda94cafa
xyronic15 <xyronb@gmail.com>
2021-09-25 15:46:11 -0400

    created data loader

---
+++ dataloader.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+import yfinance as yf
+
+# date range for collecting data from START to TODAY
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
+def load_data(ticker):
+    data =  yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
