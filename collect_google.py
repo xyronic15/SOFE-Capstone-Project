756f1cd95ae631fd52b1f273df83b6df5ba86f14
Janahan10 <janahan.ravichandran@gmail.com>
2021-09-29 19:58:02 -0400

    Added functions for plotting data, as well as finding hammers in candlesticks

--- collect_google.py
+++ collect_google.py
@@ -1,7 +1,20 @@
 from dataloader import load_data
+from plotting import plot_data
+from identify_pattern import search
 
 # get pandas dataframe of apple history
 google = load_data('GOOGL')
 
 # display top 5 rows
-print(google.head(5))
+#print(google.iloc[0, 4])
+
+#plot_data(google)
+search(google)
+
+
+
+
+
+
+
+
