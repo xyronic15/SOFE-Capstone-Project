8c6f2e3dd686d874361f93c2b7a876cd1c48bf3c
xyronic15 <xyronb@gmail.com>
2021-09-30 14:31:18 -0400

    checked search on apple

--- collect_apple.py
+++ collect_apple.py
@@ -1,7 +1,11 @@
 from dataloader import load_data
+from plotting import plot_data
+from identify_pattern import search
 
 # get pandas dataframe of apple history
 apple = load_data('AAPL')
 
 # display top 5 rows
-print(apple.head(5))
+# print(apple.head(5))
+
+search(apple)
