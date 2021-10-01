17409cc1b149298b7f13b51d2f141c6f888b6570
xyronic15 <xyronb@gmail.com>
2021-10-01 16:48:16 -0400

    load all function - update with more companies

--- dataloader.py
+++ dataloader.py
@@ -6,8 +6,15 @@ import yfinance as yf
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
 
-
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace=True)
     return data
+
+def load_all():
+    apple = load_data('AAPL')
+    google = load_data('GOOGL')
+    amazon = load_data('AMZN')
+    tesla = load_data('TSLA')
+
+    return apple, google, amazon, tesla
