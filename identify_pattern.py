066f0ec033095db79497ae550a319e053866b3c4
Janahan10 <janahan.ravichandran@gmail.com>
2021-10-07 12:24:49 -0400

    Created seperate files for hammer and inverse hammer patterns, and changed previous depth from 14 to 15

--- identify_pattern.py
+++ identify_pattern.py
@@ -8,18 +8,17 @@ Data Frame:
     5 => adj close
 """
 import pandas
+from hammer_pattern import hammer
+from inverse_hammer_pattern import inv_hammer
 
 # depth for determining previous trend direction
-PREV_DEPTH = 15
+PREV_DEPTH = 14
 # depth for determining upcoming trend direction
 FUTURE_DEPTH = 5
 
 
 # iterating through stock data to identify paterns
 def search(df):
-
-    # add new column for pattern classification
-    df['Pattern'] = ""
 
     # dictionary to hold candlesticks that fit a pattern
     classified = {}
@@ -37,59 +36,8 @@ def search(df):
         if downtrend:
             if is_hammer:
                 hammer_count += 1
-                df.iloc[idx, 7] = 'Hammer'
-                classified[idx] = 'Hammer'
             if is_inv_hammer:
                 inv_hammer_count += 1
-                df.iloc[idx, 7] = 'Inverted Hammer'
-                classified[idx] = 'Inverted Hammer'
-
-        if df.iloc[idx, 7] == '':
-            df.iloc[idx, 7] = 'No Pattern'
-
-    df.to_csv('pattern.csv')
-
-    print('inverted hammers: ', inv_hammer_count)
-    print('hammers: ', hammer_count)
-
-
-# checks if candlestick matches hammer pattern
-def hammer(i, data, body_avg):
-
-    body_hi = max(data['Close'], data['Open'])
-    body_lo = min(data['Close'], data['Open'])
-    body = body_hi - body_lo
-    small_body = bool(body < body_avg)
-    down_shadow = body_lo - data['Low']
-    up_shadow = data['High'] - body_hi
-    factor = 2.0
-    shadow_percent = 5.0
-    has_up_shadow = up_shadow > shadow_percent / 100 * body
-
-    if (small_body and body and body_lo > (data['High'] + data['Low']) / 2
-            and down_shadow >= factor * body and not has_up_shadow):
-        return True
-
-    return False
-
-
-def inv_hammer(i, data, body_avg):
-
-    body_hi = max(data['Close'], data['Open'])
-    body_lo = min(data['Close'], data['Open'])
-    body = body_hi - body_lo
-    small_body = bool(body < body_avg)
-    down_shadow = body_lo - data['Low']
-    up_shadow = data['High'] - body_hi
-    factor = 2.0
-    shadow_percent = 5.0
-    has_up_shadow = up_shadow > shadow_percent / 100 * body
-
-    if (small_body and body and body_hi < (data['High'] + data['Low']) / 2
-            and up_shadow >= factor * body and not down_shadow):
-        return True
-
-    return False
 
 
 # returns the moving average of the last n candlesticks
