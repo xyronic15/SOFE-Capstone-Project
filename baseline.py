2e0933874a1bdb7fba9a77a217beb846d32a092e
xyronic15 <xyronb@gmail.com>
2021-11-27 18:48:38 -0500

    new baseline test and graphs

--- baseline.py
+++ baseline.py
@@ -1,27 +1,34 @@
-from datetime import date
+import datetime
 import pandas as pd
 import yfinance as yf
 from dataloader import load_data
+import random
+import numpy as np
 
 # date range for collecting data from START to TODAY
-START = "2019-03-24"
-END = "2019-04-25"
+START = datetime.date(2015, 1, 1)
+END = datetime.date.today()
 
 stock_data = {}
+dates = []
 
 def load_all():
     
     companies = pd.read_csv('bats_symbols.csv', sep=",")
     for i in range(len(companies)):
         print("Loading stock " + companies.iloc[i,0])
-        stock_data[companies.iloc[i,0]] = load_data(companies.iloc[i,0], START, END)
+        stock_data[companies.iloc[i,0]] = load_data(companies.iloc[i,0], START.strftime("%Y-%m-%d"), END.strftime("%Y-%m-%d"))
 
-def get_data(df):
+def get_data(df, start):
 
     # dict to be exported to csv
     data = []
 
-    idx = 0
+    if df.loc[df['Date']==np.datetime64(start)].any().all():
+        idx = int(df.index[df['Date']==np.datetime64(start)][0])
+        print(idx)
+    else:
+        return data
 
     data.append({'Date': df.iloc[idx, 0], 'Closing Price': df.iloc[idx, 4],
                 'High after 1 day': df.iloc[min(idx+1, len(df)-1), 2], 'High after 2 days': df.iloc[min(idx+2, len(df)-1), 2],
@@ -34,17 +41,36 @@ def get_data(df):
     
     return data
 
+def generate_dates():
+
+    time_between_dates = END - START
+    days_between_dates = time_between_dates.days
+
+    for i in range(500):
+        random_number_days = random.randrange(days_between_dates)
+        random_start_date = START + datetime.timedelta(days=random_number_days)
+        random_end_date = random_start_date + datetime.timedelta(days=31)
+
+        dates.append((random_start_date, random_end_date))
 
 def main():
 
-    load_all()
-
     data = []
 
-    for company_name, df in stock_data.items():
-        if not df.empty:
-            data.extend(get_data(df))
-            print("Processing " + company_name + "...")
+    load_all()
+    generate_dates()
+
+    for start, end in dates:
+        for company_name, df in stock_data.items():
+            if not df.empty:
+                print("Processing " + company_name + " from date " + start.strftime("%Y-%m-%d"))
+                new_data = get_data(df, start)
+                if new_data:
+                    data.extend(new_data)
+                else:
+                    print("Could not get data...")
+
+    print("Saving CSV file...")
     
     final_data = pd.DataFrame(data)
     final_data.to_csv("baseline.csv")
